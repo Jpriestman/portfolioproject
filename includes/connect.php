145661afdcf181f20a2a4b5a7cbc6afa87d888c1
Jordan Priestman <jordanpriestman@Jordans-MBP.fc.ca>
2018-12-10 12:22:35 -0500

    Final touches

--- includes/connect.php
+++ includes/connect.php
@@ -1,12 +1,12 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "db_portfolioproject";
+$username = "jtpcr754_Ty2gJ2";
+$password = "usher4663";
+$dbname = "jtpcr754_db_priestmanportfolio";
 
 try {
-    $pdo = new PDO("mysql:host=$servername;dbname=db_portfolioproject", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=jtpcr754_db_priestmanportfolio", $username, $password);
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
